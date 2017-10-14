98b9483099e68a19585f972bebdcb5227307cf58
YusukeKokubo <yusuke.kokubo@gmail.com>
2017-10-15 00:56:52 +0900

    get photos from google strage

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package phantomtype
 
 import (
+	"encoding/json"
 	"log"
 
 	"cloud.google.com/go/storage"
@@ -9,10 +10,22 @@ import (
 
 	"google.golang.org/appengine"
 	"fmt"
+	"strings"
 )
 
 func init() {
 	http.HandleFunc("/hello", handler)
+}
+
+type Photos struct {
+	Name string `json:"name"`
+	Photos []Photo `json:"photos"`
+}
+
+type Photo struct {
+	key string  `json:"key"`
+	Name string `json:"name"`
+	Size int64  `json:"size"`
 }
 
 func handler(w http.ResponseWriter, r *http.Request)  {
@@ -20,18 +33,15 @@ func handler(w http.ResponseWriter, r *http.Request)  {
 
 	bucketName := "phantomtype-180814.appspot.com"
 
-	fmt.Fprint(w, "uho")
-
 	client, err := storage.NewClient(ctx)
 	if err != nil {
 		log.Fatalf("Failed to create client: %V", err)
 	}
 
 	bucket := client.Bucket(bucketName)
-	fmt.Fprint(w, bucket.Object("photos"))
 
 	objects := bucket.Objects(ctx, nil)
-	var list string = ""
+	photos := []Photo{}
 	for {
 		o, err := objects.Next()
 		if err == iterator.Done {
@@ -40,8 +50,13 @@ func handler(w http.ResponseWriter, r *http.Request)  {
 		if err != nil {
 			return
 		}
-		list = list + o.Name
+		if strings.HasSuffix(o.Name, "_tmb.jpg") {
+			photo := Photo{o.CustomerKeySHA256,o.Name, o.Size}
+			photos = append(photos, photo)
+		}
 	}
+	result := Photos{Name:"hoge", Photos:photos}
+	j, err := json.Marshal(result)
+	fmt.Fprint(w, string(j))
 
-	fmt.Fprint(w, list)
 }
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,28 @@
 import React from 'react'
+import 'isomorphic-fetch'
 
 import Layout from '../components/layout'
 import Logomark from '../resouces/images/logomark-white.svg'
 
-export default () => (
+const photosA = (photos) => (
+  photos.photos.map((p) =>
+    <div key={p.size} className="mdc-layout-grid__cell">
+      <img className="tmb" src={"https://storage.googleapis.com/phantomtype-180814.appspot.com/" + p.name} />
+    </div>
+  )
+)
+
+const Index = ({ photos }) => (
   <Layout title="PHANTOM TYPE">
     <section className="splash">
         <img className="splash"
              src="https://storage.googleapis.com/phantomtype-180814.appspot.com/splash/splash-1.jpg"/>
-      <h1>PHANTOM TYPE <Logomark/><span className="soon">coming soon...</span></h1>
+      <h1>PHANTOM TYPE <Logomark/></h1>
     </section>
     <section>
       <div className="mdc-layout-grid">
         <div className="mdc-layout-grid__inner">
-          <div className="mdc-layout-grid__cell">
-            <img className="tmb" src="https://storage.googleapis.com/phantomtype-180814.appspot.com/photos/kyoto/DSCF0369_tmb.jpg" />
-          </div>
-          <div className="mdc-layout-grid__cell">
-            <img className="tmb" src="https://storage.googleapis.com/phantomtype-180814.appspot.com/photos/kyoto/DSCF0966_tmb.jpg" />
-          </div>
-          <div className="mdc-layout-grid__cell">
-            <img className="tmb" src="https://storage.googleapis.com/phantomtype-180814.appspot.com/photos/kyoto/DSCF1066_tmb.jpg" />
-          </div>
-          <div className="mdc-layout-grid__cell">
-            <img className="tmb" src="https://storage.googleapis.com/phantomtype-180814.appspot.com/photos/kyoto/DSCF1183_tmb.jpg" />
-          </div>
-          <div className="mdc-layout-grid__cell">
-            <img className="tmb" src="https://storage.googleapis.com/phantomtype-180814.appspot.com/photos/kyoto/DSCF1205_tmb.jpg" />
-          </div>
-          <div className="mdc-layout-grid__cell">
-            <img className="tmb" src="https://storage.googleapis.com/phantomtype-180814.appspot.com/photos/kyoto/DSCF1225_tmb.jpg" />
-          </div>
+          {photosA(photos)}
         </div>
       </div>
     </section>
@@ -51,12 +43,21 @@ export default () => (
         color: #fff;
         font-size: 6.52768vw;
       }
-      .soon {
-        font-size: 1.5vw;
-      }
       img.tmb {
         max-width: 92vw;
       }
     `}</style>
   </Layout>
 )
+
+Index.getInitialProps = async ({ req }) => {
+  // const baseUrl = `${req.protocol}://${req.get('Host')}`;
+  console.log("hello")
+  const baseUrl = "http://localhost:8080"
+  const res = await fetch(baseUrl + "/hello")
+  const json = await res.json()
+  console.log(json)
+  return { photos: json }
+}
+
+export default Index
