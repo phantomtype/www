7510d52ca70643232d5f1bfaf24a6e1217cb42b0
YusukeKokubo <yusuke.kokubo@gmail.com>
2017-10-19 23:14:50 +0900

    update index

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,20 @@ import Layout from '../components/layout'
 import Logomark from '../resouces/images/logomark-white.svg'
 
 const photosA = (photos) => (
-  photos.photos.map((p) =>
-    <div key={p.size} className="mdc-layout-grid__cell">
+  photos.photos.map((p, i) =>
+      <div className="mdc-layout-grid__inner">
+    <div className="mdc-layout-grid__cell mdc-layout-grid__cell--span-4">
       <img className="tmb" src={"https://storage.googleapis.com/phantomtype-180814.appspot.com/" + p.name} />
+    </div>
+        <div className="mdc-layout-grid__cell">
+          <p>X-Pro2</p>
+        </div>
+        <style jsx>{`
+      img.tmb {
+        width: 100%;
+        right: 0;
+      }
+    `}</style>
     </div>
   )
 )
@@ -20,10 +31,8 @@ const Index = ({ photos }) => (
       <h1>PHANTOM TYPE <Logomark/></h1>
     </section>
     <section>
-      <div className="mdc-layout-grid">
-        <div className="mdc-layout-grid__inner">
+      <div className="mdc-layout-grid mdc-layout-grid--fixed-column-width mdc-layout-grid--align-right">
           {photosA(photos)}
-        </div>
       </div>
     </section>
     <style jsx>{`
@@ -43,17 +52,13 @@ const Index = ({ photos }) => (
         color: #fff;
         font-size: 6.52768vw;
       }
-      img.tmb {
-        max-width: 92vw;
-      }
     `}</style>
   </Layout>
 )
 
 Index.getInitialProps = async ({ req }) => {
-  // const baseUrl = `${req.protocol}://${req.get('Host')}`;
-  console.log("hello")
-  const baseUrl = "http://localhost:8080"
+  const baseUrl = "https://phantomtype.com"
+  // const baseUrl = "http://localhost:8080"
   const res = await fetch(baseUrl + "/hello")
   const json = await res.json()
   console.log(json)
