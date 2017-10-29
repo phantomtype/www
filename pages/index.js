3b5b9be87affff6edf7a4acf1a9cca755adcdb6d
YusukeKokubo <yusuke.kokubo@gmail.com>
2017-10-29 12:52:27 +0900

    modify index.js to Class

--- pages/index.js
+++ pages/index.js
@@ -38,19 +38,30 @@ const photosA = (photos) => (
   )
 )
 
-const Index = ({photos}) => (
-  <Layout title="PHANTOM TYPE">
-    <section className="splash">
-      <img className="splash"
-           src="https://storage.googleapis.com/phantomtype-180814.appspot.com/splash/splash-1.jpg"/>
-      <h1>PHANTOM TYPE <Logomark/></h1>
-    </section>
-    <section>
-      <div className="photos">
-        {photosA(photos)}
-      </div>
-    </section>
-    <style jsx>{`
+export default class Index extends React.Component {
+  static async getInitialProps () {
+    // const baseUrl = "https://phantomtype.com"
+    const baseUrl = "http://localhost:8080"
+    const res = await fetch(baseUrl + "/hello")
+    const json = await res.json()
+    console.log(json)
+    return {photos: json}
+  }
+
+  render () {
+    return (
+      <Layout title="PHANTOM TYPE">
+        <section className="splash">
+          <img className="splash"
+               src="https://storage.googleapis.com/phantomtype-180814.appspot.com/splash/splash-1.jpg"/>
+          <h1>PHANTOM TYPE <Logomark/></h1>
+        </section>
+        <section>
+          <div className="photos">
+            {photosA(this.props.photos)}
+          </div>
+        </section>
+        <style jsx>{`
       section.splash {
         width: 100%;
         height: 100vh;
@@ -71,16 +82,8 @@ const Index = ({photos}) => (
         margin: 0 50px;
       }
     `}</style>
-  </Layout>
-)
-
-Index.getInitialProps = async ({req}) => {
-  // const baseUrl = "https://phantomtype.com"
-  const baseUrl = "http://localhost:8080"
-  const res = await fetch(baseUrl + "/hello")
-  const json = await res.json()
-  console.log(json)
-  return {photos: json}
+      </Layout>
+    )
+  }
 }
 
-export default Index
