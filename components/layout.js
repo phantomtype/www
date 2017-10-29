1758395cad999949fddc1d98f9dfd7af62cc8d6b
YusukeKokubo <yusuke.kokubo@gmail.com>
2017-10-29 12:31:11 +0900

    drop material-components

--- components/layout.js
+++ components/layout.js
@@ -1,16 +1,12 @@
 import React from 'react'
 import Head from 'next/head'
 
-import style from 'styles/main.scss'
-
-
 export default (props) => (
   <div>
     <Head>
       <title>{props.title}</title>
       <meta charSet='utf-8'/>
       <meta name='viewport' content='initial-scale=1.0, width=device-width'/>
-      <style dangerouslySetInnerHTML={{__html: style}}></style>
     </Head>
     {props.children}
   </div>
