25154104c05c8d20b64de3a3066057cce59c0e2f
YusukeKokubo <yusuke.kokubo@gmail.com>
2017-11-06 18:25:06 +0900

    add normalize.css

--- components/layout.js
+++ components/layout.js
@@ -7,6 +7,7 @@ export default (props) => (
       <title>{props.title}</title>
       <meta charSet='utf-8'/>
       <meta name='viewport' content='initial-scale=1.0, width=device-width'/>
+      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css" />
     </Head>
     {props.children}
   </div>
