f37918119f085c3037ceec836d94593bf2ffd866
YusukeKokubo <yusuke.kokubo@gmail.com>
2017-11-13 23:22:20 +0900

    get photos devided

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,29 @@ import PhotosA from "../components/photo";
 class Photos extends React.Component {
   render() {
     return (
-      this.props.photos.photos.map((p, i) => {
-        const src = "https://storage.googleapis.com/phantomtype-180814.appspot.com/" + p.name;
-        return <PhotosA key={i} photo={p} src={src} align={i % 2} />;
-      })
+      <section className="photo-container">
+        <h3>{this.props.name}</h3>
+        {
+          this.props.photos.photos.map((p, i) => {
+            const src = "https://storage.googleapis.com/phantomtype-180814.appspot.com/" + p.name;
+            return <PhotosA key={i} photo={p} src={src} align={i % 2} />;
+          })
+        }
+        <style jsx>{`
+        section.photo-container {
+          margin: 120px 0;
+        }
+        h3 {
+          font-size: 3vw;
+          text-transform: uppercase;
+        }
+        @media (max-width: 600px){
+          section.photo-container {
+            margin: 20px 0;
+          }
+        }
+        `}</style>
+      </section>
     );
   }
 }
@@ -25,13 +44,24 @@ Photos.propTypes = {
 
 class Index extends React.Component {
   static async getInitialProps () {
+    const kibune = await Index.getPhotos("kyoto/kibune");
+    const kurama = await Index.getPhotos("kyoto/kurama");
+    const nijojo = await Index.getPhotos("kyoto/nijojo");
+    const shimogamo = await Index.getPhotos("kyoto/shimogamo");
+    const kamigamo = await Index.getPhotos("kyoto/kamigamo");
+    const kamogawa = await Index.getPhotos("kyoto/kamogawa");
+
+    return {photos: {kibune, kurama, nijojo, shimogamo, kamigamo, kamogawa}};
+  }
+
+  static async getPhotos(src: string) {
     // const baseUrl = "https://phantomtype.com";
     const baseUrl = "http://localhost:8080"
-    const res = await fetch(baseUrl + "/hello?d=kyoto/kibune");
+    const res = await fetch(baseUrl + "/hello?d=" + src);
     const json = await res.json();
     console.log(baseUrl);
     console.log(json);
-    return {photos: json};
+    return json;
   }
 
   scroll() {
@@ -52,11 +82,14 @@ class Index extends React.Component {
           <button onClick={this.scroll} className="arrow">▽</button>
         </section>
         <section className="photo-container">
-          <h2>KYOTO - 京都 <span className="desc">THE HISTORY OF JAPAN.</span></h2>
-          <h3>KIBUNE 貴船</h3>
-          <div className="photos">
-            <Photos photos={this.props.photos} />
-          </div>
+          <h2>KYOTO 京都</h2>
+          <p className="desc">THE HISTORY OF JAPAN.</p>
+          <Photos name="KIBUNE 貴船" photos={this.props.photos.kibune} />
+          <Photos name="KURAMA 蔵馬" photos={this.props.photos.kurama} />
+          <Photos name="NIJO-JO 二条城" photos={this.props.photos.nijojo} />
+          <Photos name="SHIMOGAMO 下鴨" photos={this.props.photos.shimogamo} />
+          <Photos name="KAMIGAMO 上賀茂" photos={this.props.photos.kamigamo} />
+          <Photos name="KAMOGAWA 鴨川" photos={this.props.photos.kamogawa} />
         </section>
         <style jsx>{`
       section.splash {
@@ -108,16 +141,10 @@ class Index extends React.Component {
         margin: 30px 0;
       }
       .desc {
-        font-size: 2.0vw;
+        text-align: center;
+        font-size: 2.5vw;
+        font-weight: 300;
         color: #999;
-      }
-      h3 {
-        font-size: 3vw;
-      }
-      @media (max-width: 600px){
-        div.photo-container {
-          margin: 20px 0;
-        }
       }
     `}</style>
       </Layout>
