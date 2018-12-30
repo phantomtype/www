3709a690afd947895c2f011155e8b025876c74d5
YusukeKokubo <yusuke.kokubo@gmail.com>
2018-12-30 09:41:53 +0900

    refactoring

--- pages/index.js
+++ pages/index.js
@@ -66,72 +66,41 @@ class Photos extends React.Component {
 Photos.propTypes = {
   photos: PropTypes.object,
   c: PropTypes.string,
-  p: PropTypes.string
+  p: PropTypes.string,
+  name: PropTypes.string
 };
 
-class Kanazawa extends React.Component {
+class PhotoContainer extends React.Component {
   render() {
     return (
       <section className="photo-container">
-        <h2>KANAZAWA 金沢</h2>
-        <p className="desc">The Jewel of Japan.</p>
-        <Photos name="21st Century Museum" c="kanazawa" p="21stCenturyMuseum" />
-        <Photos name="HIGASHIYAMA 東山" c="kanazawa" p="higashiyama" />
-        <Photos name="KANAZAWA STATION 金沢駅" c="kanazawa" p="kanazawa-station" />
-        <Photos name="KANAZAWA-JO 金沢城" c="kanazawa" p="kanazawajo" />
+        <h2>{this.props.city}</h2>
+        <p className="desc">{this.props.description}</p>
+        {this.props.children}
         <style jsx>{`
-      .photo-container {
-        margin: 150px 20px;
-      }
-      h2 {
-        text-align: center;
-        font-size: 5vw;
-        margin: 30px 0;
-      }
-      .desc {
-        text-align: center;
-        font-size: 2.5vw;
-        font-weight: 300;
-        color: #999;
-      }
-        `}</style>
+          .photo-container {
+            margin: 150px 20px;
+          }
+          h2 {
+            text-align: center;
+            font-size: 5vw;
+            margin: 30px 0;
+          }
+          .desc {
+            text-align: center;
+            font-size: 2.5vw;
+            font-weight: 300;
+            color: #999;
+          }
+          `}</style>
       </section>
     );
   }
 }
 
-class Kyoto extends React.Component {
-  render() {
-    return (
-      <section className="photo-container">
-        <h2>KYOTO 京都</h2>
-        <p className="desc">THE HISTORY OF JAPAN.</p>
-        <Photos name="KIBUNE 貴船" c="kyoto" p="kibune" />
-        <Photos name="KURAMA 蔵馬" c="kyoto" p="kurama" />
-        <Photos name="NIJO-JO 二条城" c="kyoto" p="nijojo" />
-        <Photos name="SHIMOGAMO 下鴨" c="kyoto" p="shimogamo" />
-        <Photos name="KAMIGAMO 上賀茂" c="kyoto" p="kamigamo" />
-        <Photos name="KAMOGAWA 鴨川" c="kyoto" p="kamogawa" />
-        <Photos name="YOSHIDA-JINJA 吉田神社" c="kyoto" p="yoshida" />
-        <style jsx>{`
-      .photo-container {
-        margin: 150px 20px;
-      }
-      h2 {
-        text-align: center;
-        font-size: 5vw;
-        margin: 30px 0;
-      }
-      .desc {
-        text-align: center;
-        font-size: 2.5vw;
-        font-weight: 300;
-        color: #999;
-      }
-        `}</style>
-      </section>
-    );
-  }
+PhotoContainer.propTypes = {
+  city: PropTypes.string,
+  description: PropTypes.string
 }
 
 class Index extends React.Component {
@@ -152,8 +121,21 @@ class Index extends React.Component {
           </div>
           <button onClick={this.scroll} className="arrow">▽</button>
         </section>
-        <Kanazawa/>
-        <Kyoto/>
+        <PhotoContainer city={`KANAZAWA 金沢`} description={`The Jewel of Japan`}>
+          <Photos name="21st Century Museum" c="kanazawa" p="21stCenturyMuseum" />
+          <Photos name="HIGASHIYAMA 東山" c="kanazawa" p="higashiyama" />
+          <Photos name="KANAZAWA STATION 金沢駅" c="kanazawa" p="kanazawa-station" />
+          <Photos name="KANAZAWA-JO 金沢城" c="kanazawa" p="kanazawajo" />
+        </PhotoContainer>
+        <PhotoContainer city={`KYOTO 京都`} description={`The History of Japan`}>
+          <Photos name="KIBUNE 貴船" c="kyoto" p="kibune" />
+          <Photos name="KURAMA 蔵馬" c="kyoto" p="kurama" />
+          <Photos name="NIJO-JO 二条城" c="kyoto" p="nijojo" />
+          <Photos name="SHIMOGAMO 下鴨" c="kyoto" p="shimogamo" />
+          <Photos name="KAMIGAMO 上賀茂" c="kyoto" p="kamigamo" />
+          <Photos name="KAMOGAWA 鴨川" c="kyoto" p="kamogawa" />
+          <Photos name="YOSHIDA-JINJA 吉田神社" c="kyoto" p="yoshida" />
+        </PhotoContainer>
         <style jsx>{`
       section.splash {
         width: 100%;
