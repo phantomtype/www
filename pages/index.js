43723c1cb55134ea0b6f78f3f7f78632431bf1c7
YusukeKokubo <yusuke.kokubo@gmail.com>
2019-01-01 17:26:31 +0900

    add nagoya

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,19 @@ import Place from "../components/place";
 import City from "../components/city";
 
 class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {cities: ['kyoto', 'kanazawa', 'nagoya']};
+  }
+
   scroll() {
     const scroll = Scroll.animateScroll;
     scroll.scrollTo(screen.height, {duration: 500, delay: 100, smooth: true});
+  }
+
+  clickCity(city) {
+    console.log(city)
+    this.setState({cities: [city]});
   }
 
   render () {
@@ -35,21 +45,42 @@ class Index extends React.Component {
           </div>
           <button onClick={this.scroll} className="arrow">▽</button>
         </section>
-        <City city={`KANAZAWA 金沢`} description={`The Jewel of Japan`}>
-          <Place name="21st Century Museum" c="kanazawa" p="21stCenturyMuseum" />
-          <Place name="HIGASHIYAMA 東山" c="kanazawa" p="higashiyama" />
-          <Place name="KANAZAWA STATION 金沢駅" c="kanazawa" p="kanazawa-station" />
-          <Place name="KANAZAWA-JO 金沢城" c="kanazawa" p="kanazawajo" />
-        </City>
-        <City city={`KYOTO 京都`} description={`The History of Japan`}>
-          <Place name="KIBUNE 貴船" c="kyoto" p="kibune" />
-          <Place name="KURAMA 蔵馬" c="kyoto" p="kurama" />
-          <Place name="NIJO-JO 二条城" c="kyoto" p="nijojo" />
-          <Place name="SHIMOGAMO 下鴨" c="kyoto" p="shimogamo" />
-          <Place name="KAMIGAMO 上賀茂" c="kyoto" p="kamigamo" />
-          <Place name="KAMOGAWA 鴨川" c="kyoto" p="kamogawa" />
-          <Place name="YOSHIDA-JINJA 吉田神社" c="kyoto" p="yoshida" />
-        </City>
+        <section className="SelectCity">
+          <button type="button" onClick={this.clickCity.bind(this, 'kyoto')}>KYOTO</button>
+          <button type="button" onClick={this.clickCity.bind(this, 'kanazawa')}>KANAZAWA</button>
+          <button type="button" onClick={this.clickCity.bind(this, 'nagoya')}>NAGOYA</button>
+        </section>
+        {
+          this.state.cities.indexOf('kanazawa') != -1 ?
+            <City city={`KANAZAWA 金沢`} description={`The Jewel of Japan`}>
+              <Place name="21st Century Museum" c="kanazawa" p="21stCenturyMuseum" />
+              <Place name="HIGASHIYAMA 東山" c="kanazawa" p="higashiyama" />
+              <Place name="KANAZAWA STATION 金沢駅" c="kanazawa" p="kanazawa-station" />
+              <Place name="KANAZAWA-JO 金沢城" c="kanazawa" p="kanazawajo" />
+            </City>
+            : null
+        }
+        {
+          this.state.cities.indexOf('kyoto') != -1 ?
+            <City city={`KYOTO 京都`} description={`The History of Japan`}>
+              <Place name="KIBUNE 貴船" c="kyoto" p="kibune" />
+              <Place name="KURAMA 蔵馬" c="kyoto" p="kurama" />
+              <Place name="NIJO-JO 二条城" c="kyoto" p="nijojo" />
+              <Place name="SHIMOGAMO 下鴨" c="kyoto" p="shimogamo" />
+              <Place name="KAMIGAMO 上賀茂" c="kyoto" p="kamigamo" />
+              <Place name="KAMOGAWA 鴨川" c="kyoto" p="kamogawa" />
+              <Place name="YOSHIDA-JINJA 吉田神社" c="kyoto" p="yoshida" />
+            </City>
+            : null
+        }
+        {
+          this.state.cities.indexOf('nagoya') != -1 ?
+            <City city={`NAGOYA 名古屋`} description={`The Center of Japan`}>
+              <Place name="SAKAE 栄" c="nagoya" p="sakae" />
+              <Place name="HOSHIGAOKA 星ヶ丘" c="nagoya" p="hoshigaoka" />
+            </City>
+            : null
+        }
         <style jsx>{`
       section.splash {
         width: 100%;
@@ -76,7 +107,11 @@ class Index extends React.Component {
         font-size: 5.52768vw;
         font-weight: 200;
       }
-      .arrow {
+      button.arrow {
+        background-color: initial;
+        cursor: pointer;
+        border: none;
+
         position: absolute;
         font-size: 4vw;
         bottom: 10vh;
@@ -86,10 +121,20 @@ class Index extends React.Component {
         cursor: pointer;
         transform: translate3d(-50%, 0, 0);
       }
-      button {
-        background-color: initial;
+      .SelectCity {
+        margin: 60px;
+        display: flex;
+        justify-content: space-around;
+      }
+      .SelectCity button {
+        background-color: #000;
+        color: #fff;
+        padding: 15px 30px;
+
         cursor: pointer;
-        border: none;
+      }
+      .SelectCity button:hover {
+        background-color: #222;
       }
     `}</style>
         <footer>(C) PHANTOM TYPE.   <a href="https://twitter.com/phantomtype" rel="noopener noreferrer" target="_blank">Contact us</a></footer>
@@ -103,6 +148,7 @@ class Index extends React.Component {
         width: 25vw;
       }
       footer {
+        color: #fff;
         text-align: center;
         margin: 50px 0;
       }
