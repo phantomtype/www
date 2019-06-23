984e6b568750e2f8bf918e4d47e21e03710b434f
YusukeKokubo <yusuke.kokubo@gmail.com>
2019-06-23 17:14:54 +0900

    add favicon

--- pages/index.js
+++ pages/index.js
@@ -74,6 +74,7 @@ class Index extends React.Component {
           <title>PHANTOM TYPE</title>
           <meta charSet='utf-8'/>
           <meta name='viewport' content='initial-scale=1.0, width=device-width'/>
+          <link rel="icon" type="image/x-icon" href="../static/favicon.ico" />
           <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css" />
           <link rel="stylesheet" href="http://fonts.googleapis.com/earlyaccess/notosansjp.css"/>
         </Head>
